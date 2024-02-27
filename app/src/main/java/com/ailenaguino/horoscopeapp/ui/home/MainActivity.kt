63635f91bd00232d3649afd5d9a481f5c5b286bb
DESKTOP-6LDNJGR\54113 <ailenaguino@gmail.com>
2024-02-26 21:18:23 -0300

    navigation

--- app/src/main/java/com/ailenaguino/horoscopeapp/ui/home/MainActivity.kt
+++ app/src/main/java/com/ailenaguino/horoscopeapp/ui/home/MainActivity.kt
@@ -2,12 +2,17 @@ package com.ailenaguino.horoscopeapp.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.ailenaguino.horoscopeapp.R
 import com.ailenaguino.horoscopeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController //Es la clase padre que gestiona toda la navegacion a traves del nav component
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +20,17 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+
+        initUI()
+    }
+
+    private fun initUI() {
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNavView.setupWithNavController(navController)
     }
 }
